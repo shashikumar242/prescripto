6762f6438a602210115b1a19b7dff7ed5ae02028
shashikumar242 <shashikumar.g.242@gmail.com>
2025-02-21 13:00:56 +0530

    fix: render server changes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: process.env.PORT || 5174, host: "0.0.0.0" },
-  allowedHosts: ["https://prescripto-admin-53x1.onrender.com"],
+  server: {
+    allowedHosts: ["https://prescripto-admin-53x1.onrender.com"],
+  },
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: process.env.PORT || 5173, host: "0.0.0.0" },
-  allowedHosts: ["prescripto-frontend-rj4l.onrender.com"],
+  server: {
+    allowedHosts: ["https://prescripto-frontend-rj4l.onrender.com/"],
+  },
 });
