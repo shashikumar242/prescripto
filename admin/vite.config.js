b6c4f7b6a4b7d0f0b5458a9f0fe9f4744df2d1f1
shashikumar242 <shashikumar.g.242@gmail.com>
2025-02-21 12:39:35 +0530

    feat: add render server credentials

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,4 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: process.env.PORT || 5174, host: "0.0.0.0" },
+  allowedHosts: ["https://prescripto-admin-53x1.onrender.com"],
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: process.env.PORT || 5173, host: "0.0.0.0" },
+  allowedHosts: ["prescripto-frontend-rj4l.onrender.com"],
 });
